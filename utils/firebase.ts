90a129fb54cb6823caf997b530eed0f67e51d6ac
«aleksandrics» <aliaksandraleanovich@gmail.com>
2022-10-04 18:11:06 +0300

    feat(app):add utilits

---
+++ utils/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDiiDTibTQ2AyzxIUfzvoENSp6TWwKSia0",
+    authDomain: "whitepeace-c133e.firebaseapp.com",
+    projectId: "whitepeace-c133e",
+    storageBucket: "whitepeace-c133e.appspot.com",
+    messagingSenderId: "541287767086",
+    appId: "1:541287767086:web:a5323419186d470bf593bf",
+};
+
+export const app = initializeApp(firebaseConfig);
