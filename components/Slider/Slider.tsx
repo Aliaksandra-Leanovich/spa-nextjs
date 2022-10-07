55c1e43ecdc875927e649c2d6895fba941e8a351
«aleksandrics» <aliaksandraleanovich@gmail.com>
2022-10-07 14:03:24 +0300

    feat(slider): add slider

---
+++ components/Slider/Slider.tsx
@@ -0,0 +1,169 @@
+import React from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+import quotesImage from "/public/Quote.svg";
+import Avatar1 from "/public/avatar1.svg";
+import Avatar2 from "/public/avatar2.svg";
+import Avatar3 from "/public/avatar3.svg";
+import "swiper/css";
+import "swiper/css/pagination";
+import SwiperCore, { EffectCoverflow, Pagination } from "swiper";
+
+import {
+  BlockReview,
+  ContainerReviews,
+  QuotesImage,
+  Review,
+  ReviewerBlock,
+  ReviewerImage,
+  ReviewerInformation,
+  ReviewerName,
+  ReviewerPosition,
+  TextReview,
+  Container,
+  ContainerMobile,
+} from "./style";
+
+const config = [
+  {
+    comment:
+      "Whitepate is designed as a collaboration tool for businesses that is a full project management solution.",
+    icon: quotesImage.src,
+    avatar: Avatar1.src,
+    name: "Oberon Shaw, MCH",
+    position: "Head of Talent Acquisition, North America",
+  },
+  {
+    comment:
+      "Whitepate is designed as a collaboration tool for businesses that is a full project management solution.",
+    icon: quotesImage.src,
+    avatar: Avatar2.src,
+    name: "Oberon Shaw, MCH",
+    position: "Head of Talent Acquisition, North America",
+  },
+  {
+    comment:
+      "Whitepate is designed as a collaboration tool for businesses that is a full project management solution.",
+    icon: quotesImage.src,
+    avatar: Avatar3.src,
+    name: "Oberon Shaw, MCH",
+    position: "Head of Talent Acquisition, North America",
+  },
+];
+
+SwiperCore.use([EffectCoverflow, Pagination]);
+
+export const Slider = () => {
+  return (
+    <>
+      <Container>
+        <Swiper
+          effect={"coverflow"}
+          grabCursor={true}
+          centeredSlides={true}
+          slidesPerView={"auto"}
+          coverflowEffect={{
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: false,
+          }}
+          spaceBetween={30}
+          pagination={{
+            clickable: true,
+          }}
+          className="swiper"
+          modules={[Pagination]}
+        >
+          <SwiperSlide>
+            <ContainerReviews>
+              {config.map((review, index) => (
+                <Review key={index}>
+                  <BlockReview>
+                    <QuotesImage src={review.icon} />
+                    <TextReview>{review.comment}</TextReview>
+                  </BlockReview>
+                  <ReviewerBlock>
+                    <ReviewerImage src={review.avatar} />
+                    <ReviewerInformation>
+                      <ReviewerName>{review.name}</ReviewerName>
+                      <ReviewerPosition>{review.position}</ReviewerPosition>
+                    </ReviewerInformation>
+                  </ReviewerBlock>
+                </Review>
+              ))}
+            </ContainerReviews>
+          </SwiperSlide>
+          <SwiperSlide>
+            <ContainerReviews>
+              {config.map((review, index) => (
+                <Review key={index}>
+                  <BlockReview>
+                    <QuotesImage src={review.icon} />
+                    <TextReview>{review.comment}</TextReview>
+                  </BlockReview>
+                  <ReviewerBlock>
+                    <ReviewerImage src={review.avatar} />
+                    <ReviewerInformation>
+                      <ReviewerName>{review.name}</ReviewerName>
+                      <ReviewerPosition>{review.position}</ReviewerPosition>
+                    </ReviewerInformation>
+                  </ReviewerBlock>
+                </Review>
+              ))}
+            </ContainerReviews>
+          </SwiperSlide>
+          <SwiperSlide>
+            <ContainerReviews>
+              {config.map((review, index) => (
+                <Review key={index}>
+                  <BlockReview>
+                    <QuotesImage src={review.icon} />
+                    <TextReview>{review.comment}</TextReview>
+                  </BlockReview>
+                  <ReviewerBlock>
+                    <ReviewerImage src={review.avatar} />
+                    <ReviewerInformation>
+                      <ReviewerName>{review.name}</ReviewerName>
+                      <ReviewerPosition>{review.position}</ReviewerPosition>
+                    </ReviewerInformation>
+                  </ReviewerBlock>
+                </Review>
+              ))}
+            </ContainerReviews>
+          </SwiperSlide>
+        </Swiper>
+      </Container>
+      <ContainerMobile>
+        <Swiper
+          slidesPerView={"auto"}
+          spaceBetween={30}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[Pagination]}
+          className="mySwiper"
+        >
+          {config.map((review, index) => (
+            <SwiperSlide key={index}>
+              <Review>
+                <BlockReview>
+                  <QuotesImage src={review.icon} />
+                  <TextReview>{review.comment}</TextReview>
+                </BlockReview>
+                <ReviewerBlock>
+                  <ReviewerImage src={review.avatar} />
+                  <ReviewerInformation>
+                    <ReviewerName>{review.name}</ReviewerName>
+                    <ReviewerPosition>{review.position}</ReviewerPosition>
+                  </ReviewerInformation>
+                </ReviewerBlock>
+              </Review>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </ContainerMobile>
+    </>
+  );
+};
---
+++ components/Slider/style.ts
@@ -0,0 +1,89 @@
+import styled from "@emotion/styled";
+import { Colors } from "../../ui/colors";
+import { media } from "../../ui/media";
+import { typography } from "../../ui/typography";
+
+export const Container = styled.div`
+  display: flex;
+  width: 100%;
+  ${media.TABLET} {
+    display: none;
+  }
+`;
+export const ContainerReviews = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  column-gap: 32px;
+  justify-content: center;
+  align-items: center;
+`;
+export const Review = styled.div`
+  padding: 60px 24px;
+  background-color: ${Colors.LIGHTBLUE};
+  border-radius: 10px;
+
+  ${media.PHONE} {
+    padding: 20px;
+  }
+`;
+export const BlockReview = styled.div`
+  border-bottom: 1px solid ${Colors.WHITE};
+  margin-bottom: 60px;
+`;
+export const QuotesImage = styled.img`
+  width: 86px;
+  margin-bottom: 32px;
+`;
+export const TextReview = styled.p`
+  margin-bottom: 40px;
+  ${typography.bodytext}
+  color: ${Colors.WHITE};
+`;
+
+export const ReviewerBlock = styled.div`
+  display: flex;
+  ${media.TABLET} {
+    flex-direction: column;
+    align-items: flex-start;
+  }
+`;
+export const ReviewerImage = styled.img`
+  width: 95px;
+  height: 95px;
+  margin-right: 42px;
+  ${media.TABLET} {
+    margin-right: 0px;
+    margin-bottom: 24px;
+  }
+`;
+export const ReviewerInformation = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  ${media.TABLET} {
+    align-items: flex-start;
+  }
+`;
+export const ReviewerName = styled.p`
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 600;
+  font-size: 24px;
+  line-height: 36px;
+  color: ${Colors.BLUE};
+  ${media.TABLET} {
+    margin-bottom: 24px;
+  }
+`;
+export const ReviewerPosition = styled.p`
+  ${typography.subtitle};
+  color: ${Colors.WHITE};
+`;
+export const ContainerMobile = styled.div`
+  display: none;
+  width: 100%;
+  ${media.TABLET} {
+    display: flex;
+  }
+`;
