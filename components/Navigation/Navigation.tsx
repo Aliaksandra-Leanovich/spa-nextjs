d8e018d307afc6232ebb257d4a718e8adca2bab1
«aleksandrics» <aliaksandraleanovich@gmail.com>
2022-10-07 13:46:51 +0300

    feat(nav):add navigation

--- components/Navigation/Navigation.tsx
+++ components/Navigation/Navigation.tsx
@@ -1,44 +1,109 @@
-import React from "react";
-import { StyledNavigation, StyledLink } from "./styles";
-import ArrowIcon from "/public/icons/arrowUp.svg";
+import React, { FormEvent, useState } from "react";
+import {
+  StyledNavigation,
+  StyledLink,
+  TextLink,
+  Arrow,
+  NavigationContainer,
+  StyledRightNavigation,
+  ContainerLinks,
+  ContainerButtons,
+} from "./styles";
+import { app } from "../../utils/firebase";
+import { getAuth, signOut } from "firebase/auth";
+import ArrowIcon from "/public/icons/arrow.svg";
 import Link from "next/link";
+import router from "next/router";
+import { routes } from "../../routes/routes";
+import { LinkTemplate } from "../LinkTemplate/LinkTemplate";
+import { Burger } from "../Burger/Burger";
+import { ButtonTemplate } from "../ButtonTemplate/ButtonTemplate";
 
 const config = [
   {
-    href: "/test",
+    href: "#products",
     icon: ArrowIcon.src,
     title: "Products",
   },
   {
-    href: "/",
+    href: "#solutions",
     icon: ArrowIcon.src,
     title: "Solutions",
   },
   {
-    href: "/",
+    href: "#resources",
     icon: ArrowIcon.src,
     title: "Resources",
   },
   {
-    href: "/",
+    href: "#pricing",
     icon: ArrowIcon.src,
     title: "Pricing",
   },
 ];
 
 export const Navigation = () => {
+  const [open, setOpen] = useState<boolean>(false);
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const auth = getAuth(app);
+    signOut(auth)
+      .then(() => {
+        localStorage.removeItem("authUser");
+        router.push(routes.SIGNIN);
+      })
+      .catch((error) => {
+        // An error happened.
+      });
+  };
   return (
-    <StyledNavigation>
-      {config.map((link, index) => (
-        <Link href={link.href} key={index}>
-          <a>
-            <span>{link.title}</span>
-            <div>
-              <img src={link.icon} alt={"arrow"} />
-            </div>
-          </a>
-        </Link>
-      ))}
-    </StyledNavigation>
+    <NavigationContainer>
+      <StyledNavigation>
+        {config.map((link, index) => (
+          <Link href={link.href} key={index}>
+            <StyledLink>
+              <TextLink>{link.title}</TextLink>
+
+              <Arrow src={link.icon} alt={"arrow"} />
+            </StyledLink>
+          </Link>
+        ))}
+        <form onSubmit={handleSubmit}>
+          <ButtonTemplate
+            color="lightblue"
+            type="submit"
+            text="Logout"
+            background="yellow"
+          />
+        </form>
+        <LinkTemplate href="/whitepacefree" text="Try Whitepace free" />
+      </StyledNavigation>
+
+      <Burger open={open} setOpen={setOpen} />
+      <StyledRightNavigation open={open}>
+        <ContainerLinks>
+          {config.map((link, index) => (
+            <Link href={link.href} key={index}>
+              <StyledLink>
+                <TextLink>{link.title}</TextLink>
+                <Arrow src={link.icon} alt={"arrow"} />
+              </StyledLink>
+            </Link>
+          ))}
+        </ContainerLinks>
+        <ContainerButtons>
+          <form onSubmit={handleSubmit}>
+            <ButtonTemplate
+              color="lightblue"
+              type="submit"
+              text="Logout"
+              background="yellow"
+            />
+          </form>
+          <LinkTemplate href="/whitepacefree" text="Try Whitepace free" />
+        </ContainerButtons>
+      </StyledRightNavigation>
+    </NavigationContainer>
   );
 };
--- components/Navigation/styles.ts
+++ components/Navigation/styles.ts
@@ -1,22 +1,77 @@
 import styled from "@emotion/styled";
 import { Colors } from "../../ui/colors";
 import { typography } from "../../ui/typography";
-import Link from "next/link";
+import { media } from "../../ui/media";
+
+interface IProps {
+  open: boolean;
+}
 
 const StyledNavigation = styled.nav`
   display: flex;
   justify-content: space-around;
   align-items: center;
-  width: 100%;
-  max-width: 555px;
+
+  ${media.LAPTOP} {
+    display: none;
+  }
 `;
-const StyledLink = styled(Link)`
-  ${typography.subtitle}
-  color: ${Colors.WHITE};
-  text-decoration: none;
-  max-width: 117px;
+const StyledLink = styled.span`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  margin-right: 40px;
 `;
-export { StyledNavigation, StyledLink };
+const TextLink = styled.span`
+  ${typography.subtitle}
+  color: ${Colors.WHITE};
+  cursor: pointer;
+  &:hover {
+    color: ${Colors.YELLOW};
+  }
+`;
+const Arrow = styled.img`
+  width: 9px;
+  margin-left: 20px;
+`;
+export const StyledRightNavigation = styled.div<IProps>`
+  background: ${Colors.BLACK};
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+
+  padding: 2rem;
+  position: absolute;
+  top: 0;
+  right: 0;
+  transition: transform 0.3s ease-in-out;
+
+  transform: ${({ open }) => (open ? "translateX(0)" : "translateX(100%)")};
+
+  ${media.PHONE} {
+    width: 100%;
+  }
+`;
+export const ContainerLinks = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  ${media.LAPTOP} {
+    display: grid;
+    row-gap: 2rem;
+  }
+`;
+
+export const ContainerButtons = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const NavigationContainer = styled.div`
+  display: flex;
+`;
+
+export { StyledNavigation, StyledLink, TextLink, Arrow, NavigationContainer };
