e8357d054543679ec6248bfe364835ec5deaf708
«aleksandrics» <aliaksandraleanovich@gmail.com>
2022-10-07 14:01:25 +0300

    feat(link): add template for links

---
+++ components/LinkTemplate/LinkTemplate.tsx
@@ -0,0 +1,44 @@
+import styled from "@emotion/styled";
+import Link from "next/link";
+import React from "react";
+import { Colors } from "../../ui/colors";
+import { typography } from "../../ui/typography";
+import ArrowRightIcon from "public/icons/arrow-right.svg";
+
+interface IButton {
+  href: string;
+  text: string;
+}
+
+export const LinkTemplate = ({ href, text }: IButton) => {
+  return (
+    <Link href={href}>
+      <StyledLink>
+        {text} <Arrow src={ArrowRightIcon.src} alt={"arrow"} />
+      </StyledLink>
+    </Link>
+  );
+};
+
+const StyledLink = styled.a`
+  background-color: ${Colors.LIGHTBLUE};
+  color: ${Colors.WHITE};
+  ${typography.button}
+  border: none;
+  border-radius: 8px;
+  padding: 20px;
+
+  cursor: pointer;
+  width: max-content;
+  transition: all 0.5s ease-out;
+  &:hover {
+    background-color: ${Colors.YELLOW};
+    transform: translateY(-2px);
+  }
+`;
+
+const Arrow = styled.img`
+  width: 10px;
+
+  /* margin-left: 20px; */
+`;
