c041ad6b071bf3d3ff36d4ffa9d62d497adff429
«aleksandrics» <aliaksandraleanovich@gmail.com>
2022-10-12 10:35:36 +0300

    feat(sign in):add password required

--- components/SignInForm/SignInForm.tsx
+++ components/SignInForm/SignInForm.tsx
@@ -24,6 +24,7 @@ interface IUserLocal {
 }
 const validationSchema = Yup.object().shape({
   email: Yup.string().required("Email is required").email("Email is invalid"),
+  password: Yup.string().required("Password is required"),
 });
 
 export const SignInForm = () => {
