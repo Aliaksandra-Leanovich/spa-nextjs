fdf548735128396f23b5e4c433465b08e63c9d8e
«aleksandrics» <aliaksandraleanovich@gmail.com>
2022-10-14 16:34:49 +0300

    feat(app):add header

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,35 +7,39 @@ import { MainSection } from "../components/Sections/MainSection";
 import { ManagementSection } from "../components/Sections/ManagementSection";
 import { ExtensionSection } from "../components/Sections/ExtensionSection";
 import { CustomiseSection } from "../components/Sections/CustomiseSection";
+import { EverywhereSection } from "../components/Sections/EverywhereSection";
 import { DataSection } from "../components/Sections/DataSection";
+import { SponsorsSection } from "../components/Sections/SponsorsSection";
 import { FavoriteSection } from "../components/Sections/FavoriteSection";
 import { ClientsSection } from "../components/Sections/ClientsSection";
 import { TodaySection } from "../components/Sections/TodaySection";
-import { EverywhereSection } from "../components/Sections/EverywhereSection";
-import { SponsorsSection } from "../components/Sections/SponsorsSection";
+import { Header } from "../components/Header/Header";
 
 const Home: NextPage = () => {
   return (
-    <Container>
-      <MainSection />
-      <ManagementSection />
-      <ExtensionSection />
-      <CustomiseSection />
-      <PricingBlock />
-      <EverywhereSection />
-      <DataSection />
-      <SponsorsSection />
-      <FavoriteSection />
-      <ClientsSection />
-      <TodaySection />
-      <Footer />
-    </Container>
+    <>
+      <Container>
+        <Header />
+        <MainSection />
+        <ManagementSection />
+        <ExtensionSection />
+        <CustomiseSection />
+        <PricingBlock />
+        <EverywhereSection />
+        <DataSection />
+        <SponsorsSection />
+        <FavoriteSection />
+        <ClientsSection />
+        <TodaySection />
+        <Footer />
+      </Container>
+    </>
   );
 };
 
 export default Home;
 
-const Container = styled.div`
+export const Container = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
