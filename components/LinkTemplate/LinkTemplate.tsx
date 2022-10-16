069e69301d715ccb56817ee66f3618a2ce128204
«aleksandrics» <aliaksandraleanovich@gmail.com>
2022-10-16 13:00:17 +0300

    fix(link):remove custom paddings and add variants

--- components/LinkTemplate/LinkTemplate.tsx
+++ components/LinkTemplate/LinkTemplate.tsx
@@ -1,50 +1,26 @@
-import styled from "@emotion/styled";
 import Link from "next/link";
 import React from "react";
-import { Colors } from "../../ui/colors";
-import { typography } from "../../ui/typography";
 import ArrowRightIcon from "public/icons/arrow-right.svg";
+import { Arrow, LinkSC } from "./styles";
 
-interface ILink {
-  href: string;
-  text: string;
-  padding?: string;
+export enum LinkVariants {
+  linkSmall = "linkSmall",
+  linkMedium = "linkMedium",
+  linkLarge = "linkLarge",
 }
 
-type LinkStyle = Pick<ILink, "padding">;
+export interface ILink {
+  href: string;
+  text: string;
+  variant: LinkVariants;
+}
 
-export const LinkTemplate = ({ href, text, padding }: ILink) => {
+export const LinkTemplate = ({ href, text, variant }: ILink) => {
   return (
     <Link href={href}>
-      <StyledLink padding={padding}>
+      <LinkSC variant={variant}>
         {text} <Arrow src={ArrowRightIcon.src} alt={"arrow"} />
-      </StyledLink>
+      </LinkSC>
     </Link>
   );
 };
-
-const StyledLink = styled.a<LinkStyle>`
-  padding: ${({ padding }) => padding};
-  width: max-content;
-
-  border: none;
-  border-radius: 8px;
-  background-color: ${Colors.LIGHTBLUE};
-  color: ${Colors.WHITE};
-  ${typography.button}
-
-  z-index: 1;
-
-  cursor: pointer;
-
-  transition: background-color 0.5s ease-out;
-
-  &:hover {
-    background-color: ${Colors.YELLOW};
-  }
-`;
-
-const Arrow = styled.img`
-  width: 10px;
-  margin-left: 10px;
-`;
