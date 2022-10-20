3cfc6d6f4cfe6171155ade05cd8749166cfdfab6
«aleksandrics» <aliaksandraleanovich@gmail.com>
2022-10-20 12:38:18 +0300

    fest(input): add types to file

--- components/Input/Input.tsx
+++ components/Input/Input.tsx
@@ -1,22 +1,6 @@
-import React, { InputHTMLAttributes } from "react";
-import { FieldError, FieldValues } from "react-hook-form";
-import { Path, UseFormRegister } from "react-hook-form";
+import React from "react";
 import { ContainerSC, ErrorMessageSC, StyledInputSC } from "./style";
-
-interface IFormValues {
-  email: string;
-  password: string;
-  name?: string;
-  errors?: string;
-}
-
-interface IRegister extends FieldValues, IFormValues {}
-
-interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
-  label: Path<IFormValues>;
-  register: UseFormRegister<IRegister>;
-  errors: FieldError | undefined;
-}
+import { InputProps } from "./types";
 
 export const Input = ({
   type,
