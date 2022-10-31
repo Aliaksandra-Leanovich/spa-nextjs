8f1b1800729b01da692a4752b1d252eb72357877
«aleksandrics» <aliaksandraleanovich@gmail.com>
2022-10-31 15:34:17 +0300

    fix(typography): fix types

--- ui/Typography.tsx
+++ ui/Typography.tsx
@@ -1,16 +1,18 @@
 import { css } from "@emotion/react";
 import styled from "@emotion/styled";
+import React from "react";
+import { VariantsTypography } from "../enums/TypographyVariants";
 
-export interface ITypography {
+export interface IProps {
+  variant: VariantsTypography;
+  color: string;
+  children: string;
+}
+interface ITypography {
   color?: string;
-  variant: string;
+  variant: VariantsTypography;
 }
-
-export interface IObjectKeys {
-  [key: string]: string;
-}
-
-export const TypesTypography: IObjectKeys = {
+export const TypographyTypes = {
   h1: "h1",
   h2: "h2",
   h3: "h3",
@@ -23,101 +25,81 @@ export const TypesTypography: IObjectKeys = {
   paragraphMedium: "p",
   paragraphMediumBold: "p",
   subtitle: "p",
+} as const;
+
+const variantTypography = {
+  h1: css`
+    font-family: "Inter";
+    font-weight: 700;
+    font-size: 56px;
+    line-height: 64px;
+  `,
+  h2: css`
+    font-family: "Inter";
+    font-weight: 700;
+    font-size: 64px;
+    line-height: 77px;
+    letter-spacing: -0.02em;
+  `,
+  h3: css`
+    font-family: "Inter";
+    font-weight: 700;
+    font-size: 36px;
+    line-height: 44px;
+  `,
+  h5: css`
+    font-family: "Inter";
+    font-weight: 700;
+    font-size: 28px;
+    line-height: 36px;
+  `,
+  paragraphSmall: css`
+    font-family: "Inter";
+    font-weight: 400;
+    font-size: 18px;
+    line-height: 30px;
+  `,
+  paragraphXS: css`
+    font-family: "Inter";
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 20px;
+  `,
+  paragraphSmallBold: css`
+    font-family: "Inter";
+    font-weight: 700;
+    font-size: 18px;
+    line-height: 30px;
+  `,
+  paragraphMedium: css`
+    font-family: "Inter";
+    font-weight: 400;
+    font-size: 24px;
+    line-height: 36px;
+  `,
+  paragraphMediumBold: css`
+    font-family: "Inter";
+    font-weight: 400;
+    font-size: 24px;
+    line-height: 36px;
+  `,
+  subtitle: css`
+    font-family: "Inter";
+    font-weight: 400;
+    font-size: 18px;
+    line-height: 30px;
+  `,
 };
 
-export interface IProps {
-  variant: string;
-  color: string;
-  children: string;
-}
-
-import { media } from "./media";
-
-const variantTypography = (variant = TypesTypography.subtitle) =>
-  ({
-    h1: css`
-      font-family: "Inter";
-      font-weight: 700;
-      font-size: 56px;
-      line-height: 64px;
-      ${media.PHONE} {
-        font-size: 32px;
-        line-height: 44px;
-      }
-    `,
-    h2: css`
-      font-family: "Inter";
-      font-weight: 700;
-      font-size: 64px;
-      line-height: 77px;
-      letter-spacing: -0.02em;
-
-      ${media.TABLET} {
-        font-size: 54px;
-        line-height: 65px;
-      }
-      ${media.PHONE} {
-        font-size: 36px;
-        line-height: 44px;
-      }
-    `,
-    h3: css`
-      font-family: "Inter";
-      font-weight: 700;
-      font-size: 36px;
-      line-height: 44px;
-    `,
-    h5: css`
-      font-family: "Inter";
-      font-weight: 700;
-      font-size: 28px;
-      line-height: 36px;
-    `,
-    paragraphSmall: css`
-      font-family: "Inter";
-      font-weight: 400;
-      font-size: 18px;
-      line-height: 30px;
-    `,
-    paragraphXS: css`
-      font-family: "Inter";
-      font-weight: 400;
-      font-size: 16px;
-      line-height: 20px;
-    `,
-    paragraphSmallBold: css`
-      font-family: "Inter";
-      font-weight: 700;
-      font-size: 18px;
-      line-height: 30px;
-    `,
-    paragraphMedium: css`
-      font-family: "Inter";
-      font-weight: 400;
-      font-size: 24px;
-      line-height: 36px;
-    `,
-    paragraphMediumBold: css`
-      font-family: "Inter";
-      font-weight: 400;
-      font-size: 24px;
-      line-height: 36px;
-    `,
-    subtitle: css`
-      font-family: "Inter";
-      font-weight: 400;
-      font-size: 18px;
-      line-height: 30px;
-    `,
-  }[variant]);
-
 export const TypographyRoot = styled("p")<ITypography>`
-  ${({ variant }) => variantTypography(variant)}
+  ${({ variant }) => variantTypography[variant]}
   color: ${({ color }) => color};
 `;
 
 export const Typography = ({ variant, color, children }: IProps) => {
-  const Component: any = variant ? TypesTypography[variant] : "p";
+  const Component = variant
+    ? TypographyTypes[variant]
+    : ("p" as React.ElementType<any>);
 
   return (
     <TypographyRoot as={Component} color={color} variant={variant}>
